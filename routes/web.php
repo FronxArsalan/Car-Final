8a3c8383cce2989092b06f80ce8fe22e0a6aad5f
FronxArsalan <fnonxsolutions.arsalan@gmail.com>
2025-04-21 19:16:43 +0500

    arsalana latest

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,19 @@ Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 // authenticate
 Route::middleware('authenticate')->group(function () {
     // dashboard
+
+
+    
     Route::get('/', [AdminController::class, 'index'])->name('dashboard');
     Route::resource('tires', TireController::class);
     Route::resource('products', ProductController::class);
 
     Route::get('/tire/inventory', [TireController::class, 'inventory'])->name('tires.inventory');
     // search
+
+
+
+    
     Route::get('/tire/search', [TireController::class, 'search'])->name('tires.search');
     Route::get('tire/import', [TireController::class, 'showImportForm'])->name('tires.import.form');
     Route::post('tire/import', [TireController::class, 'import'])->name('tires.import');
